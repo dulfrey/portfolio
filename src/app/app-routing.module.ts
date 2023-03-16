acd45adf6e6f9ff1342c47ffb534bba17a64c4d8
dulfrey <wdhernandezh@unal.edu.co>
2023-03-15 21:40:14 -0500

    contact + social links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product.details/product.details.component';
 
@@ -8,6 +9,7 @@ import { ProductDetailsComponent } from './product.details/product.details.compo
 const routes: Routes = [
   { path: 'proyect/:id', component: ProductDetailsComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', component: HomeComponent }
 ];
 
